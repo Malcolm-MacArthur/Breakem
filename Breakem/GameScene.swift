2ddeaca90c4ac55572ac96b59bea5cb12891c1b7
Malcolm <malco.macarthur@gmail.com>
2015-10-24 11:30:11 -0600

    Testing
    if this works on my other computer, I will use gitHub for now on.

--- Breakem/GameScene.swift
+++ Breakem/GameScene.swift
@@ -11,12 +11,11 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        let blueBlock = [SKSpriteNode](count: 4, repeatedValue: self.childNodeWithName("BlueBlock") as! SKSpriteNode)
+        blueBlock[0].xScale = 2
+        blueBlock[1].xScale = 0.5
+        blueBlock[2].xScale = 2
+        blueBlock[3].xScale = 2
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -24,18 +23,7 @@ class GameScene: SKScene {
         
         for touch in touches {
             let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+
         }
     }
    
